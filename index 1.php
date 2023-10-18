5313c2a3629c88b8e615f52e78bc312c86660cfb
zawadirugenyi <zawadirugenyi@gmail.com>
2023-10-18 16:39:33 +0300

    first commits

---
+++ index 1.php
@@ -0,0 +1,487 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Start Meta -->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="description" content="Hostily - Luxury Hotel Booking HTML5 Template"/>
+	<meta name="keywords" content="Creative, Digital, multipage, landing, freelancer template"/>
+	<meta name="author" content="ThemeOri">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Title of Site -->
+	<title>Hostily - Luxury Hotel Booking HTML5 Template</title>
+	<!-- Favicons -->
+	<link rel="icon" type="image/png" href="assets/img/favicon.png">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- Font Awesome CSS -->
+	<link rel="stylesheet" href="assets/css/all.css">
+	<!-- Animate CSS -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!-- Nice Select CSS -->
+	<link rel="stylesheet" href="assets/css/nice-select.css">
+	<!-- Swiper Bundle CSS -->
+	<link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+	<!-- Magnific Popup CSS -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<!-- Mean Menu CSS -->
+	<link rel="stylesheet" href="assets/css/meanmenu.min.css">
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="assets/sass/style.css"> 
+</head>
+
+<body>
+	<!-- Preloader start -->
+	<div class="theme-loader">
+		<div class="spinner">
+			<div class="double-bounce1"></div>
+			<div class="double-bounce2"></div>
+		</div>
+	</div>
+	<!-- Preloader end -->
+	<!-- Header Area Start -->
+	<div class="header__sticky">
+		<div class="header__area">
+			<div class="container custom__container">
+				<div class="header__area-menubar">
+					<div class="header__area-menubar-left">
+						<div class="header__area-menubar-left-logo">
+							<a href="index.html"><img src="assets/img/logo.png" alt=""></a>
+							<div class="responsive-menu"></div>
+						</div>
+					</div>
+					<div class="header__area-menubar-right">
+						<div class="header__area-menubar-right-menu menu-responsive">						
+							<ul id="mobilemenu">
+								<li class="menu-item-has-children"><a href="#">Home</a>
+									
+								</li>
+								<li class="menu-item-has-children"><a href="#">Pages</a>
+									<ul class="sub-menu">
+										<li><a href="about.html">About</a></li>
+										<li><a href="services-team.html">Team</a></li>
+										<li><a href="services-details.html">Services Details</a></li>
+										<li><a href="coming-soon.html">Coming Soon</a></li>
+									</ul>
+								</li>
+								<li class="menu-item-has-children"><a href="#">Room</a>
+									<ul class="sub-menu">
+										<li><a href="room-style.html">Ground Floor</a></li>
+										<li><a href="room-modern.html">First Floor</a></li>
+										<li><a href="room-list.html">Second Floor</a></li>
+										
+									</ul>
+								</li>
+								
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="header__area-menubar-right-box">
+						<div class="header__area-menubar-right-box-btn">
+							<a class="theme-btn" href="contact.html">Book Now<i class="fal fa-long-arrow-right"></i></a>
+	
+						</div>
+						<div class="header__area-menubar-right-box">
+						<div class="header__area-menubar-right-box-btn">
+							<a class="theme-btn" href="contact.html">Login<i class="fal fa-long-arrow-right"></i></a>
+						</div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Header Area End -->	
+	<!-- Banner Area Start -->	
+	<div class="banner__area" data-background="assets/img/">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="banner__area-title">
+						<h1>The Best Hotel<span>Deals in the World</span></h1>
+						
+								<img class="img__full" src="assets/img/photo_6010393430800317779_y.jpg" alt="">
+						
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<form action="#">
+					<div class="check__area">
+						<div class="check__area-item">						
+							<p>Check In<input id="date" type="date"></p>								
+						</div>
+						<div class="check__area-item">						
+							<p>Check Out<input id="date2" type="date"></p>								
+						</div>
+						<div class="check__area-item">						
+							<div class="check__area-item-room">
+								<p>Room</p>
+								<select name="select">
+									<option value="1">1 Room</option>
+									<option value="2">2 Room</option>
+									<option value="3">3 Room</option>
+									<option value="4">4 Room</option>
+									<option value="5">5 Room</option>
+								</select>
+							</div>								
+						</div>
+						<div class="check__area-item button">
+							<button class="theme-btn" type="submit">Check Now</button>							
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- Banner Area End -->
+	<!-- Accommodations Area Start -->
+	<div class="accommodations__area section-padding">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-xl-5 col-lg-6 lg-mb-30">
+					<div class="accommodations__area-title">
+						<span class="subtitle__one">Accommodations</span>
+						<h2>Welcome Our Hotels And Resorts</h2>
+						<p>Savvy travelers are looking for more than just the next destination on the map. They are looking for a memorable experience and to make new friends along the way.</p>
+						<a class="theme-btn" href="about.html">Read More<i class="fal fa-long-arrow-right"></i></a>
+					</div>
+				</div>
+				<div class="col-xl-7 col-lg-6">
+					<div class="accommodations__area-right">
+						<div class="accommodations__area-right-image">
+							<img src="assets/img/hotel/photo_6010393430800317778_y.jpg" alt="">
+							<div class="accommodations__area-right-image-two">
+								<img src="assets/img/hotel/photo_6010393430800317839_y.jpg" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Accommodations Area End -->
+	<!-- Deluxe Area Start -->
+	<div class="deluxe__area section-padding">
+		<div class="container">
+			<div class="row align-items-end mb-60">
+				<div class="col-xl-5">
+					<div class="deluxe__area-title">
+						<span class="subtitle__one">Deluxe and Luxury</span>
+						<h2>Our Luxury Rooms</h2>
+					</div>					
+				</div>
+				<div class="col-xl-7">
+					<div class="deluxe__area-btn">
+						<ul>
+							<li class="active" data-filter="*">All Rooms</li>
+							<li data-filter=".luxury">Luxury</li>
+							<li data-filter=".single">Single</li>
+							<li data-filter=".suite">Small Suite</li>
+							<li data-filter=".family">Family</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row deluxe__area-active">
+				<div class="col-xl-3 col-lg-4 mb-30 suite">
+					<div class="deluxe__area-item"> 
+						<div class="deluxe__area-item-image">
+							<img class="img__full" src="assets/img/photo_6010393430800317779_y.jpg" alt="">
+						</div>
+						<div class="deluxe__area-item-content"> 
+							<h6><a href="#"><span>$134</span> / Night</a></h6>
+							<h4><a href="room-details.html">Small Suite</a></h4>
+							<a class="simple-btn" href="contact.html"><i class="far fa-chevron-right"></i>Booking Now</a> 
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 col-lg-8 mb-30 suite">
+					<div class="deluxe__area-item deluxe__area-item-hover"> 
+						<div class="deluxe__area-item-image">
+							<img class="img__full" src="assets/img/photo_6010393430800317783_y.jpg" alt="">
+						</div>
+						<div class="deluxe__area-item-content"> 
+							<h6><a href="#"><span>$199</span> / Night</a></h6>
+							<h4><a href="room-details.html">Deluxe Room</a></h4>
+							<a class="simple-btn" href="contact.html"><i class="far fa-chevron-right"></i>Booking Now</a> 
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-4 mb-30 family">
+					<div class="deluxe__area-item"> 
+						<div class="deluxe__area-item-image">
+							<img class="img__full" src="assets/img/photo_6010393430800317782_y.jpg" alt="">
+						</div>
+						<div class="deluxe__area-item-content"> 
+							<h6><a href="#"><span>$319</span> / Night</a></h6>
+							<h4><a href="room-details.html">Family Room</a></h4>
+							<a class="simple-btn" href="contact.html"><i class="far fa-chevron-right"></i>Booking Now</a> 
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 col-lg-8 lg-mb-30 single">
+					<div class="deluxe__area-item"> 
+						<div class="deluxe__area-item-image">
+							<img class="img__full" src="assets/img/photo_6010393430800317781_y.jpg" alt="">
+						</div>
+						<div class="deluxe__area-item-content"> 
+							<h6><a href="#"><span>$169</span> / Night</a></h6>
+							<h4><a href="room-details.html">Single Room</a></h4>
+							<a class="simple-btn" href="contact.html"><i class="far fa-chevron-right"></i>Booking Now</a> 
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 luxury">
+					<div class="deluxe__area-item"> 
+						<div class="deluxe__area-item-image">
+							<img class="img__full" src="assets/img/photo_6010393430800317780_y.jpg" alt="">
+						</div>
+						<div class="deluxe__area-item-content"> 
+							<h6><a href="#"><span>$249</span> / Night</a></h6>
+							<h4><a href="room-details.html">Luxury Room</a></h4>
+							<a class="simple-btn" href="contact.html"><i class="far fa-chevron-right"></i>Booking Now</a> 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Deluxe Area End -->
+	
+	<!-- Services Area Start -->
+	<div class="services__area section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 lg-mb-30">
+					<div class="services__area-item">
+						<div class="services__area-item-icon">
+							<img src="assets/img/icon/cleaning.png" alt="">
+						</div>
+						<div class="services__area-item-content">
+							<h5><a href="#">Room Cleaning</a></h5>
+							<p>Proin massa augue, lacinia at blandit ac, fringilla scelerisque tortor</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 sm-mb-30">
+					<div class="services__area-item">
+						<div class="services__area-item-icon">
+							<img src="assets/img/icon/wifi.png" alt="">
+						</div>
+						<div class="services__area-item-content">
+							<h5><a href="#">Room Wifi</a></h5>
+							<p>Proin massa augue, lacinia at blandit ac, fringilla scelerisque tortor</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+					<div class="services__area-item">
+						<div class="services__area-item-icon">
+							<img src="assets/img/icon/location.png" alt="">
+						</div>
+						<div class="services__area-item-content">
+							<h5><a href="#">Pickup & Drop</a></h5>
+							<p>Proin massa augue, lacinia at blandit ac, fringilla scelerisque tortor</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Services Area End -->
+	
+		
+	<!-- Testimonial Area Start -->	
+	<div class="testimonial__area section-padding pb-0"> 
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+				   <div class="testimonial__area-bg">
+					<div class="swiper testimonial__slider">
+						<div class="swiper-wrapper">
+							<div class="swiper-slide">
+								<div class="testimonial__area-item">
+									<div class="testimonial__area-item-image">
+										<img class="img__full" src="assets/img/avatar/testimonial-1.jpg" alt="">
+									</div>
+									<div class="testimonial__area-item-content">
+										<h4> Fina Mwayuma</h4>										
+										<span>BA student at Daystar</span>
+										<p>Proin massa augue, lacinia at blandit ac, fringilla scelerisque tortor. Mauris sit amet lectus porta, porta lectus non, malesuada neque. Integer in tempus leo. Quisque vitae leo ac ex suscipit iaculis eu in nisl. Donec vestibulum volutpat lectus, vel aliquet massa porttitor in. Integer eleifend</p>
+									</div>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="testimonial__area-item">
+									<div class="testimonial__area-item-image">
+										<img class="img__full" src="assets/img/avatar/testimonial-2.jpg" alt="">
+									</div>
+									<div class="testimonial__area-item-content">
+										<h4>David Prest/h4>										
+										<span> Logistic Student at Daystar</span>
+										<p>Proin massa augue, lacinia at blandit ac, fringilla scelerisque tortor. Mauris sit amet lectus porta, porta lectus non, malesuada neque. Integer in tempus leo. Quisque vitae leo ac ex suscipit iaculis eu in nisl. Donec vestibulum volutpat lectus, vel aliquet massa porttitor in. Integer eleifend</p>
+									</div>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="testimonial__area-item">
+									<div class="testimonial__area-item-image">
+										<img class="img__full" src="assets/img/avatar/testimonial-3.jpg" alt="">
+									</div>
+									<div class="testimonial__area-item-content">
+										<h4>Ray Scott</h4>										
+										<span>Acs student at Daystar</span>
+										<p>Proin massa augue, lacinia at blandit ac, fringilla scelerisque tortor. Mauris sit amet lectus porta, porta lectus non, malesuada neque. Integer in tempus leo. Quisque vitae leo ac ex suscipit iaculis eu in nisl. Donec vestibulum volutpat lectus, vel aliquet massa porttitor in. Integer eleifend</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="testimonial__area-item-dots">
+						<div class="pagination"></div>
+					</div>
+				   </div>
+				</div>
+			</div>
+		</div>
+	</div>	    
+	<!-- Testimonial Area End -->	
+
+	<!-- Footer Area Start -->	
+	<div class="footer__area">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 sm-mb-30">
+					<div class="footer__area-widget">
+						<div class="footer__area-widget-about">
+							<div class="footer__area-widget-about-logo">
+								<a href="index.html"><img src="assets/img/logo.png" alt=""></a>
+							</div>
+							<p>Phasellus nisi sapien, rutrum placerat sapien eu, rhoncus tempus</p>
+							<div class="footer__area-widget-about-social">
+								<ul>
+									<li><a href="#"><i class="fab fa-facebook-f"></i></a>
+									</li>
+									<li><a href="#"><i class="fab fa-twitter"></i></a>
+									</li>
+									<li><a href="#"><i class="fab fa-behance"></i></a>
+									</li>
+									<li><a href="#"><i class="fab fa-youtube"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 lg-mb-30">
+					<div class="footer__area-widget">
+						<h5>Information</h5>
+						<div class="footer__area-widget-contact">
+							<div class="footer__area-widget-contact-item">
+								<div class="footer__area-widget-contact-item-icon">
+									<i class="fal fa-map-marked-alt"></i>
+								</div>
+								<div class="footer__area-widget-contact-item-content">									
+									<span><a href="#">Kryptons /a></span>
+								</div>
+							</div>
+							<div class="footer__area-widget-contact-item">
+								<div class="footer__area-widget-contact-item-icon">
+									<i class="fal fa-envelope-open-text"></i>
+								</div>
+								<div class="footer__area-widget-contact-item-content">									
+									<span><a href="mailto:help.info@gamil.com">kryptons@gmail.com</a></span>
+								</div>
+							</div>
+							<div class="footer__area-widget-contact-item">
+								<div class="footer__area-widget-contact-item-icon">
+									<i class="fal fa-phone-alt"></i>
+								</div>
+								<div class="footer__area-widget-contact-item-content">									
+									<span><a href="tel:+123(458)585568">+254 114 242 782</a></span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-2 col-lg-2 col-md-5 col-sm-4 sm-mb-30">
+					<div class="footer__area-widget">
+						<h5>Pages Links</h5>
+						<div class="footer__area-widget-menu">
+							<ul>
+								<li><a href="services-details.html"><i class="fal fa-angle-double-right"></i>Room Cleaning</a></li>
+								<li><a href="services-details.html"><i class="fal fa-angle-double-right"></i>Car Parking</a></li>
+								<li><a href="services-details.html"><i class="fal fa-angle-double-right"></i>Swimming pool</a></li>
+							
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-4 col-lg-4 col-md-7 col-sm-8">
+					<div class="footer__area-widget">
+						<h5>Subscribe</h5>
+						<div class="footer__area-widget-subscribe">
+							<form action="#">
+								<input type="text" name="email" placeholder="Email Address" required="">
+								<button type="submit"><i class="fal fa-hand-pointer"></i></button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="copyright__area">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-xl-6 col-lg-6 col-md-7 md-mb-10">
+						<div class="copyright__area-left md-t-center">
+							<p>Copyright © 2023 <a href="https://themeforest.net/user/themeori/portfolio">Team</a> Website by <a href="index.html">Resident Connect</a></p>
+						</div>
+					</div>
+					<div class="col-xl-6 col-lg-6 col-md-5">
+						<div class="copyright__area-right t-right md-t-center">
+							<ul>
+								<li><a href="#">FAQ</a></li>
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Footer Area End -->	
+	<!-- Scroll Btn Start -->
+	<div class="scroll-up">
+		<svg class="scroll-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102"><path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" /> </svg>
+	</div>
+	<!-- Scroll Btn End -->
+	<!-- Main JS -->
+	<script src="assets/js/jquery-3.6.0.min.js"></script>
+	<!-- Bootstrap JS -->
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- Counter Up JS -->
+	<script src="assets/js/jquery.counterup.min.js"></script>
+	<!-- Popper JS -->
+	<script src="assets/js/popper.min.js"></script>
+	<!-- Magnific Popup JS -->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- Nice Select JS -->
+	<script src="assets/js/jquery.nice-select.min.js"></script>
+	<!-- Swiper Bundle JS -->
+	<script src="assets/js/swiper-bundle.min.js"></script>
+	<!-- Waypoints JS -->
+	<script src="assets/js/jquery.waypoints.min.js"></script>
+	<!-- Mean Menu JS -->
+	<script src="assets/js/jquery.meanmenu.min.js"></script>
+	<!-- Isotope JS -->
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<!-- Custom JS -->
+	<script src="assets/js/custom.js"></script>
+</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Boostrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="registration.css">
+    <title>Registration</title>
+</head>
+
+<body >
+    
+<script>
+    
+</script>
+
+<div class="container">
+    <h1>Welcome!</h1>
+    <form action="proc.php" method="post">
+        <div class="mb-3">
+            <label class="form-label">User Name</label>
+            <input type="text" name="username" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label c lass="form-label">Email</label>
+            <input type="email" name="email" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" required>
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary">Sign Up</button>
+    </form>
+</div>
+
+
+
+
+  
+    <!-- Bootstrap5 JS  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+
+
+    
+</body>
+
+</html>
