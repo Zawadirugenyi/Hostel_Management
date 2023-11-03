5e68d16db5ec540b95c3b89e5dd27d7bade74705
zawadirugenyi <zawadirugenyi@gmail.com>
2023-11-03 17:22:10 +0300

    save

--- home.php
+++ home.php
@@ -374,8 +374,7 @@
               <span class="subtitle__one">Jivinjari</span>
               <h2>Swimming Pool</h2>
               <p>
-                Proin massa augue, lacinia at blandit ac, fringilla
-                scelerisque tortor. Mauris sit amet lectus porta,
+                We offer the best
               </p>
               <a class="theme-border-btn" href="services-details.html">Read More<i
                   class="fal fa-long-arrow-right"></i></a>
